4ed2403c5aa61b6eea708cdd379d2c32ff87f4f7
Mia Taylor <miastaylor1@gmail.com>
2023-03-07 20:04:13 -0800

    Updated main py script, added gitignore

--- BERTLeap.py
+++ BERTLeap.py
@@ -1,41 +1,5 @@
-
-from IPython import get_ipython
-# Old: dependencies and cloned repo
-# New: Change directory to model 
-
-# ####**0.2 Download pre-trained models and place them to data/pretrained/, you could choose from [VisualBERT](https://github.com/uclanlp/visualbert), [LXMERT](https://github.com/airsplay/lxmert), [UNITER](https://github.com/ChenRocks/UNITER).**
-
-# ####**0.4 Download the visaul features extracted by BUTD. 36 2048-dimension visual feature is extracted from each CXR Image. We use this [implementation](https://github.com/airsplay/py-bottom-up-attention). This step will take a while (~1min). To save downloading time, you can also make a copy of this [shareable link](https://drive.google.com/file/d/1BFw0jc0j-ffT2PhI4CZeP3IJFZg3GxlZ/view?usp=sharing) to your own google drive and mount you colab to your gdrive.**
-
-# ***0.4.1 Load visual features***
-
-
-
-# In[ ]:
-
-
-# feature_example, bbox_example, (img_w_example, img_h_example) = openI_v_f[openI.id.iloc[0]]
-
-
-# In[ ]:
-
-
-# feature_example.shape, bbox_example.shape, (img_w_example, img_h_example)
-
-
-# ####**Now We have download all data, models, and dependencies. We are good to go!!!**
-# **1. Change default arguments**
-# 
-# First, let's check it out!
-
-# In[ ]:
-
-
 from param import args
-
-
-# In[ ]:
-
+import sys
 args.__dict__
 
 args.batch_size = 18
@@ -43,24 +7,6 @@ args.epochs = 2
 args.model = 'visualbert' # use visualbert
 args.load_pretrained = '/content/Transformers-VQA/models/pretrained/visualbert.th' #load pretrained visualbert model
 args.max_seq_length = 128 #truncate or pad report lengths to 128 subwords
-
-
-# In[ ]:
-
-
-
-
-
-# TODO: Create customized dataloader**
-
-# In[ ]:
-
-
-findings = list(openI.columns[1:-2])
-
-
-# In[ ]:
-
 
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
@@ -104,111 +50,64 @@ class OpenIDataset(Dataset):
     return new_boxes  
 
 
-# In[ ]:
 
+if __name__ == "__main__":
 
-training = OpenIDataset(df = openI, vf = openI_v_f,  split='train', model = args.model)
-testing = OpenIDataset(df = openI, vf = openI_v_f,  split='test', model = args.model)
+    training = OpenIDataset(df = openI, vf = openI_v_f,  split='train', model = args.model)
+    testing = OpenIDataset(df = openI, vf = openI_v_f,  split='test', model = args.model)
 
+    train_loader = DataLoader(training, batch_size=args.batch_size,shuffle=True, num_workers=0,drop_last=True, pin_memory=True)
+    test_loader = DataLoader(testing, batch_size=128,shuffle=False, num_workers=0,drop_last=False, pin_memory=True)
 
-# In[ ]:
+    from vqa_model import VQAModel
+    #init model
+    model = VQAModel(num_answers = len(findings), model = args.model)
 
+    model.encoder.load(args.load_pretrained)
 
-train_loader = DataLoader(training, batch_size=args.batch_size,shuffle=True, num_workers=0,drop_last=True, pin_memory=True)
-test_loader = DataLoader(testing, batch_size=128,shuffle=False, num_workers=0,drop_last=False, pin_memory=True)
+    import torch
+    loss = torch.nn.BCEWithLogitsLoss()
+    sgmd = torch.nn.Sigmoid()
 
+    from src.optimization import BertAdam
+    optim = BertAdam(list(model.parameters()),lr=args.lr,warmup=0.1,t_total=len(train_loader)*args.epochs)
 
-# ####**3. Model, Optimizer, Loss Function, and Evaluation Function**
+    # Evaluation function, we will report the AUC and accuracy of each finding
+    def eval(target, pred):
+        acc_list = []
+        for i, d in enumerate(findings[:-1]): #normal is excluded
+            acc = np.mean(target[:,i] == (pred[:,i]>=0.5))
+            print(i,d,acc)
+            acc_list.append(acc)
+        print('Averaged: '+str(np.average(acc_list)))
 
-# In[ ]:
+    from tqdm.notebook import tqdm
 
+    iter_wrapper = (lambda x: tqdm(x, total=len(train_loader))) if args.tqdm else (lambda x: x)
+    best_valid = 0
+    for epoch in range(args.epochs):
+        epoch_loss = 0
+        for i, (cxr_id, feats, boxes, report, target) in iter_wrapper(enumerate(train_loader)):
+            model.train()
+            optim.zero_grad()
+            feats, boxes, target = feats.cuda(), boxes.cuda(), target.cuda()
+            logit = model(feats, boxes, report)
+            running_loss = loss(logit, target)
+            running_loss = running_loss * logit.size(1)
+            epoch_loss += running_loss
+            running_loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 5.)
+            optim.step()
+        print("Epoch "+str(epoch)+": Training Loss: "+str(epoch_loss/len(train_loader)))
+        print('Evaluation: ')
+        model.eval()
+        logit_list, target_list = [], []
+        iter_wrapper = (lambda x: tqdm(x, total=len(test_loader)))
+        for i, (cxr_id, feats, boxes, report, target) in iter_wrapper(enumerate(test_loader)):
+            target_list.append(target)
+            with torch.no_grad():
+                feats, boxes = feats.cuda(), boxes.cuda()
+                logit = model(feats, boxes, report)
+                logit_list.append(sgmd(logit).cpu().numpy())
 
-from vqa_model import VQAModel
-#init model
-model = VQAModel(num_answers = len(findings), model = args.model)
-
-
-# In[ ]:
-
-
-#load pretrained weights
-model.encoder.load(args.load_pretrained)
-
-
-
-
-import torch
-loss = torch.nn.BCEWithLogitsLoss()
-
-
-# In[ ]:
-
-
-from src.optimization import BertAdam
-optim = BertAdam(list(model.parameters()),lr=args.lr,warmup=0.1,t_total=len(train_loader)*args.epochs)
-# t_total denotes total training steps
-# batch_per_epoch = len(train_loader)
-# t_total = int(batch_per_epoch * args.epochs)
-
-
-# In[ ]:
-
-
-# Evaluation function, we will report the AUC and accuray of each finding
-def eval(target, pred):
-    acc_list = []
-    for i, d in enumerate(findings[:-1]): #normal is excluded
-        acc = np.mean(target[:,i] == (pred[:,i]>=0.5))
-        print(i,d,acc)
-        acc_list.append(acc)
-    print('Averaged: '+str(np.average(acc_list)))
-
-
-# In[ ]:
-
-
-sgmd = torch.nn.Sigmoid()
-
-
-# ####**4. HIT and RUN**
-
-# In[ ]:
-
-
-from tqdm.notebook import tqdm
-
-iter_wrapper = (lambda x: tqdm(x, total=len(train_loader))) if args.tqdm else (lambda x: x)
-best_valid = 0
-for epoch in range(args.epochs):
-  epoch_loss = 0
-  for i, (cxr_id, feats, boxes, report, target) in iter_wrapper(enumerate(train_loader)):
-    model.train()
-    optim.zero_grad()
-    feats, boxes, target = feats.cuda(), boxes.cuda(), target.cuda()
-    logit = model(feats, boxes, report)
-    running_loss = loss(logit, target)
-    running_loss = running_loss * logit.size(1)
-    epoch_loss += running_loss
-    running_loss.backward()
-    torch.nn.utils.clip_grad_norm_(model.parameters(), 5.)
-    optim.step()
-  print("Epoch "+str(epoch)+": Training Loss: "+str(epoch_loss/len(train_loader)))
-  print('Evaluation: ')
-  model.eval()
-  logit_list, target_list = [], []
-  iter_wrapper = (lambda x: tqdm(x, total=len(test_loader)))
-  for i, (cxr_id, feats, boxes, report, target) in iter_wrapper(enumerate(test_loader)):
-    target_list.append(target)
-    with torch.no_grad():
-      feats, boxes = feats.cuda(), boxes.cuda()
-      logit = model(feats, boxes, report)
-      logit_list.append(sgmd(logit).cpu().numpy())
-
-  eval(np.concatenate(target_list,axis = 0), np.concatenate(logit_list,axis = 0))
-
-
-# In[ ]:
-
-
-
-
+        eval(np.concatenate(target_list,axis = 0), np.concatenate(logit_list,axis = 0))
