553c27f2b2c61b9731ebd187dcb5d0dccbd760ab
Ryan <ryan@ridgefield.cc>
2023-03-07 17:00:18 -0800

    Updated feature extract into module form

--- PixelHop2/feature_extract.py
+++ PixelHop2/feature_extract.py
@@ -1,25 +1,20 @@
 import os
 import pydicom
 import cv2
+import imageio as io
+import numpy as np
+from pixelhop2 import Pixelhop2
+from skimage.util import view_as_windows
 
 train_dir = "/u/scratch/e/ewolos/train"
 test_dir = "/u/scratch/e/ewolos/test"
 
-images = []
 
 def dicom_to_png(path):
     ds = pydicom.dcmread(path)
     im = ds.pixel_array
     im = cv2.resize(im, (206, 206))
     return im
-
-for file in os.scandir(train_dir):
-    filename = train_dir + file.name
-    images.append(dicom_to_png(filename))
-    break
-
-from pixelhop2 import Pixelhop2
-from skimage.util import view_as_windows
 
 # example callback function for collecting patches and its inverse
 def Shrink(X, shrinkArg):
@@ -31,24 +26,28 @@ def Shrink(X, shrinkArg):
 def Concat(X, concatArg):
     return X
 
-import imageio as io
-import numpy as np
 
-X = np.reshape(images, (len(images), 206, 206, 1))
+def extract_all_features():
 
-print(" input feature shape: %s"%str(X.shape))
+    images = []
 
-# set args
-SaabArgs = [{'num_AC_kernels': 8, 'needBias':False, 'useDC':True, 'batch':None}, 
-            {'num_AC_kernels': 8, 'needBias':True, 'useDC':True, 'batch':None}]
-shrinkArgs = [{'func':Shrink, 'win':3}, 
-              {'func': Shrink, 'win':3},]
-concatArg = {'func':Concat}
+    for file in os.scandir(train_dir):
+        filename = train_dir + file.name
+        images.append(dicom_to_png(filename))
+        break
 
-p2 = Pixelhop2(depth=1, TH1=0.1, TH2=0.00005, SaabArgs=SaabArgs, shrinkArgs=shrinkArgs, concatArg=concatArg)
-output = p2.fit(X)
-output = p2.transform(X)
-print(output[0])
-print(output[0].shape)
+    X = np.reshape(images, (len(images), 206, 206, 1))
+
+    # set args
+    SaabArgs = [{'num_AC_kernels': 8, 'needBias':False, 'useDC':True, 'batch':None}, 
+                {'num_AC_kernels': 8, 'needBias':True, 'useDC':True, 'batch':None}]
+    shrinkArgs = [{'func':Shrink, 'win':3}, 
+                {'func': Shrink, 'win':3},]
+    concatArg = {'func':Concat}
+    p2 = Pixelhop2(depth=1, TH1=0.1, TH2=0.00005, SaabArgs=SaabArgs, shrinkArgs=shrinkArgs, concatArg=concatArg)
+    output = p2.fit(X)
+    output = p2.transform(X)
+    return output[0]
+
 
 
